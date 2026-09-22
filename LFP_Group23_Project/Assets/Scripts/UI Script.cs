391f74093e808e4fc5a9252892a8c2b4ad647697
Malumlangzin <langandzunga06@gmail.com>
2026-09-22 17:19:40 +0200

    late update, pause and menu

--- LFP_Group23_Project/Assets/Scripts/UI Script.cs
+++ LFP_Group23_Project/Assets/Scripts/UI Script.cs
@@ -1,22 +1,42 @@
 using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
 using UnityEngine.SceneManagement;
 
 public class UIScript : MonoBehaviour
 {
+    [SerializeField] GameObject pauseMenu;
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            pauseMenu.SetActive(true);
+        }
+    }
+
+    public void Mainmenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
     public void StartGame()
     {
         SceneManager.LoadScene(1);
     }
 
-    public void RestartGame()
+    public void Levels()
     {
         SceneManager.LoadScene(1);
-    }
-
-    public void BackToMenu()
-    {
-
-        SceneManager.LoadScene(0);
     }
 
     public void ExitGame()
